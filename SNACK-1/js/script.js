d59bb1b46dc7b76ada55bbec899370e7a98e70e1
MarcoPicca <il96marco@gmail.com>
2023-11-17 11:49:55 +0100

    snack uno con correzione

--- SNACK-1/js/script.js
+++ SNACK-1/js/script.js
@@ -2,10 +2,18 @@
 // Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
 
 let array = [];
-let userNum = parseInt(prompt('scrivi un numero'));
-userNum.push(array);
 
 let somma = 0;
-while (userNum < 50){
 
+
+
+
+while (somma < 50){
+    let userNum = parseInt(prompt('scrivi un numero'));
+    if (!isNaN(userNum)){
+    somma += userNum;
+    array.push(userNum);
+    }   
 }
+
+console.log(array, somma);
