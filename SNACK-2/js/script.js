46172c3f7ffcddeacaed5ccba74aa92942d25391
MarcoPicca <il96marco@gmail.com>
2023-11-17 12:09:28 +0100

    inizio snack 2

---
+++ SNACK-2/js/script.js
@@ -0,0 +1,9 @@
+// Generatore di "nomi cognomi" casuali: il Grande Gatsby ha una lista di nomi e una lista di cognomi,
+// e da questi due array distinti vuole generare una falsa lista di invitati con nome e cognome.
+
+
+let name = ['angelo','germinio','aldo','mirko','carlo'];
+let surname = ['ganzi','trenoro', 'cafasso', 'molerbi', 'crattoli'];
+
+let nameSurname = [];
+
