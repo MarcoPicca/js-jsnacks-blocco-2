c677d787f3d05fbe90f7c4d9d7c8a97c7a273785
MarcoPicca <il96marco@gmail.com>
2023-11-17 15:12:33 +0100

    snack 2 completo

--- SNACK-2/js/script.js
+++ SNACK-2/js/script.js
@@ -2,8 +2,19 @@
 // e da questi due array distinti vuole generare una falsa lista di invitati con nome e cognome.
 
 
-let name = ['angelo','germinio','aldo','mirko','carlo'];
-let surname = ['ganzi','trenoro', 'cafasso', 'molerbi', 'crattoli'];
+let nameList = ['angelo','germinio','aldo','mirko','carlo'];
+let lastNameList = ['ganzi','trenoro', 'cafasso', 'molerbi', 'crattoli'];
 
 let nameSurname = [];
 
+let numberOfRandom = 15;
+
+
+for (let i=0; i < numberOfRandom; i++){
+    let firstNameGen = nameList[ Math.floor(Math.random() * nameList.length)];
+    let lastNameGen = lastNameList[ Math.floor(Math.random() * lastNameList.length)];
+    let completeNameGen = firstNameGen + ' ' + lastNameGen;
+    nameSurname.push(completeNameGen);
+}
+
+console.log(nameSurname);
