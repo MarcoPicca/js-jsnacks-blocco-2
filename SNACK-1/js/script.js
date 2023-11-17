a115ea04c4d03ece17ecc608fc10354e5960c362
MarcoPicca <il96marco@gmail.com>
2023-11-17 11:29:00 +0100

    inizio while

---
+++ SNACK-1/js/script.js
@@ -0,0 +1,11 @@
+// Crea un array vuoto e chiedi all'utente un numero da inserire nell'array.
+// Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+
+let array = [];
+let userNum = parseInt(prompt('scrivi un numero'));
+userNum.push(array);
+
+let somma = 0;
+while (userNum < 50){
+
+}
