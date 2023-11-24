76057ac2f1eb8ab7f69857ea26aeef952ab887ac
MarcoPicca <il96marco@gmail.com>
2023-11-24 12:36:45 +0100

    oggetti zucchine

---
+++ SNACK-8 OGGETTI JS/js/script.js
@@ -0,0 +1,17 @@
+// Crea 10 oggetti che rappresentano una zucchina, indicandone per ognuno varietà, peso e lunghezza. 
+// Calcola quanto pesano tutte le zucchine.
+
+
+
+const zucchine = [
+    { varieta: 'Verde', peso: 150, lunghezza: 15 },
+    { varieta: 'Gialla', peso: 180, lunghezza: 18 },
+    { varieta: 'Nera', peso: 200, lunghezza: 20 },
+    { varieta: 'Bianca', peso: 170, lunghezza: 14 },
+    { varieta: 'Trombetta', peso: 250, lunghezza: 25 },
+    { varieta: 'Tonda', peso: 130, lunghezza: 13 },
+    { varieta: 'Lunga', peso: 190, lunghezza: 19 },
+    { varieta: 'Marina', peso: 160, lunghezza: 16 },
+    { varieta: 'Montana', peso: 210, lunghezza: 21 },
+    { varieta: 'Corta', peso: 175, lunghezza: 17 }
+  ];
